7c8e7c8be5eef08d1b452f6cd2242a5a74aeb858
AlexONEX <a.schwartzmann@hotmail.com>
2022-12-24 16:25:42 -0300

    Para quantum 1 no funciona

--- main.cpp
+++ main.cpp
@@ -14,7 +14,8 @@ int main(int argc, char *argv[]){
 
     //int quantum = rand() % 9*config.cantidad_jugadores + 1; 
     //quantum<config.cantidad_jugadores ? quantum = config.cantidad_jugadores : quantum = quantum;
-	int quantum = config.cantidad_jugadores/2;
+	//int quantum = config.cantidad_jugadores/2;
+    int quantum = 1;
     // Creo equipos (lanza procesos)
     strat == USTEDES ? assert(config.cantidad_jugadores>1): assert(config.cantidad_jugadores>0);
 	Equipo rojo(&belcebu, ROJO, strat, config.cantidad_jugadores, quantum, config.pos_rojo);
