e617421866df7786e882f82bd9bb9a825a07d2f9
AlexONEX <a.schwartzmann@hotmail.com>
2022-12-25 18:18:25 -0300

    CC

---
+++ compileAndTest.sh
@@ -0,0 +1,11 @@
+#!/bin/sh
+
+# This script will remove the binary, cmake cache and make the project again
+# Then it will execute testing script
+rm -f test_tp_sistemas
+rm -rf CMakeFiles
+rm -f CMakeCache.txt
+cmake .
+cmake --build .
+make
+./testing.sh    
